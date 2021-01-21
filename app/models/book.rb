c48c047a1ddde74ffc59caa0fef4a0f3f1ce4899
github username <github email address>
2021-01-20 20:56:03 -0500

    add method to compute star average

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,10 @@ class Book < ApplicationRecord
 
   validates :title, presence: true
 
+#write method ot compute the star average
+  def update_star(review)
+    self.average_stars = self.average_stars + review.star_rating
+    self.save
+  end
+
 end

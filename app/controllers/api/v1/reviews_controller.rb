85981a26ca8eea784a2aed3a9456383139823898
github username <github email address>
2021-01-20 21:26:41 -0500

    review private method and index method

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,33 +1,33 @@
 class Api::V1::ReviewsController < ApplicationController
 
-  def index
-    @reviews = Review.all
-    render json:@reviews
+before_action :set_book
 
+
+  def index
+    @reviews = @book.reviews
+    render json: @reviews
   end
 
   def create
-    @review = Review.new(review_params)
-    if @review.save
-      render json:@review
-    else
-      render json: {error: 'Error creating review'}
-    end
+
   end
 
   def show
-    @review = Review.find(params[:id])
-    render json: @review
+
   end
 
   def destroy
-    @review = Review.find(params[:id])
-    @review.destroy
+    
   end
 
   private
+
+    def set_book
+      @book = Book.find(params[:book_id])
+    end
+
     def review_params
-      params.require(:body).permit(:star_rating, :datetime, :likes)
+      params.require(:body, :book_id).permit(:star_rating, :datetime, :likes)
     end
 
 end

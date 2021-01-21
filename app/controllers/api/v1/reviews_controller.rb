a212814ccff57e137852c47aeee505d6d9b6b4db
github username <github email address>
2021-01-20 21:16:30 -0500

    reviews control methods

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,19 +1,33 @@
 class Api::V1::ReviewsController < ApplicationController
 
   def index
+    @reviews = Review.all
+    render json:@reviews
+
   end
 
   def create
+    @review = Review.new(review_params)
+    if @review.save
+      render json:@review
+    else
+      render json: {error: 'Error creating review'}
+    end
   end
 
   def show
+    @review = Review.find(params[:id])
+    render json: @review
   end
 
   def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
   end
 
   private
     def review_params
+      params.require(:body).permit(:star_rating, :datetime, :likes)
     end
 
 end

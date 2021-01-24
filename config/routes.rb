0d0664b64164e0d5c62b04a78e6eb075ec7f1c29
github username <github email address>
2021-01-24 15:20:49 -0500

    add reviews to route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      resources :books do
       resources :reviews
-      resources :books
     end
   end
+end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
